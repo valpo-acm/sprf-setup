49fac9d3b7e8859a31d3e9507b8ed4ed79c8d2f4
Ethan <ethan.hawk@valpo.edu>
2022-10-25 17:22:12 -0500

    server.go: add go-routine for pulling in changes from the git repo

--- server/server.go
+++ server/server.go
@@ -1,11 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os/exec"
+	"time"
 )
 
+func update_repo() {
+	for {
+		_, err := exec.Command("git pull").Output()
+		if err != nil {
+			fmt.Printf("%s", err)
+		}
+		time.Sleep(1 * time.Hour)
+	}
+}
+
 func main() {
+
+	go update_repo()
+
 	port := "8100"
 	directory := "."
 
